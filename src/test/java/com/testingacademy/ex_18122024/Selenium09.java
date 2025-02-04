2ca63fb52f4f89807682349a8b13ca6b22bb183e
Ganesh <ganeshthombare0@gmail.com>
2025-02-04 09:17:40 +0530

    test 123

---
+++ src/test/java/com/testingacademy/ex_18122024/Selenium09.java
@@ -0,0 +1,37 @@
+package com.testingacademy.ex_18122024;
+
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.testng.annotations.Test;
+
+public class Selenium09 {
+
+@Test
+    public void testMethod1(){
+
+    // EdgeOptions -> It will help you set the browser
+    // option to browsers
+    // window size
+    // headless mode - there is not UI
+    // full UI mode - default - UI browser
+    // incognito mode - switch
+    // start Max
+    // add extension
+    // 100 + others, https , http
+    // Local storage, download
+
+    EdgeOptions edgeOptions = new EdgeOptions();
+      edgeOptions.addArguments("--Start-maximized");
+      edgeOptions.addArguments("--Window-size=800,600");
+//    edgeOptions.addArguments("--incognito");
+//    edgeOptions.addArguments("--headless");
+
+    EdgeDriver driver = new EdgeDriver(edgeOptions);
+    driver.get("https://google.com");
+
+
+
+}
+
+
+}
